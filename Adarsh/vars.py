7d5f424c747de5e39d7f35c434f10f3f00e174be
DevAryanxDD <144301993+DevAryanxDD@users.noreply.github.com>
2023-10-17 22:04:30 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '14766829'))
-    API_HASH = str(getenv('API_HASH', 'cfaca926290f395a7404927ccb88422d'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetoo_link_robotkk'))
+    API_ID = int(getenv('API_ID', '20315396'))
+    API_HASH = str(getenv('API_HASH', '52edde742449c75895948e0a25abe7db'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6632472902:AAFOBcGaUQxf3mPj_Dho5asfmm_1NKU6duc'))
+    name = str(getenv('name', 'fimletoolmk'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001693616773'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001750626831'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5281368582").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5196029054").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'itzrenierb'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'litenv10'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://rde33242:appleiphone@realme.7n9loel.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Liten:OoIj9FZ6J5ca7K31@cluster0.hcxotkc.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
